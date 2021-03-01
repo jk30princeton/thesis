6309563a66e63de172235a9ff129c46ca1c0c733
Joseph Kim <jk30@princeton.edu>
2021-02-28 19:15:57 -0800

    new

--- run.go
+++ run.go
@@ -2,36 +2,39 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
+	"os/exec"
 	"strings"
-
-	"github.com/scylladb/go-set/strset"
 )
 
 func main() {
-	content, err := ioutil.ReadFile("/Users/josephkim/Downloads/names.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	text := string(content)
-	split := strings.Split(text, "\n")
+	// content, err := ioutil.ReadFile("/Users/josephkim/Downloads/names.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// text := string(content)
+	// split := strings.Split(text, "\n")
 
-	for i, s := range split {
-		if s == "" {
-			continue
-		}
-		fmt.Println(i, s)
-	}
-	set1 := strset.New()
-	set1.Add("Hello")
+	// for i, s := range split {
+	// 	if s == "" {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i, s)
+	// }
 
-	set2 := strset.New()
-	set2.Add("Hello")
-	set2.Add("World")
+	command2 := exec.Command("/bin/bash", "-c", "nix-instantiate '<nixpkgs>' -A firefox")
+	command2.Dir = dir
+	out2 := strings.TrimSpace(run(command2))
+	fmt.Println(out2)
 
-	set3 := strset.Union(set1, set2)
-	set3.Add("Hello")
-	fmt.Println(set3)
-	fmt.Println(set3.Has("Hello"))
+	// set1 := strset.New()
+	// set1.Add("Hello")
+
+	// set2 := strset.New()
+	// set2.Add("Hello")
+	// set2.Add("World")
+
+	// set3 := strset.Union(set1, set2)
+	// set3.Add("Hello")
+	// fmt.Println(set3)
+	// fmt.Println(set3.Has("Hello"))
 }
